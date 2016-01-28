02e9be609cb28ac050e8007e0a112d6f7cfee16a
John Vial <john.francis.vial@gmail.com>
2016-01-28 15:39:18 +1000

    bug fix

--- sl.sh
+++ sl.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 adduser --disabled-password --gecos "" ubuntu
-adduser ubunutu sudo
+adduser ubuntu sudo
 mkdir /home/ubuntu/.ssh/
 cp /root/.ssh/authorized_keys /home/ubuntu/.ssh/authorized_keys
 chown -R ubuntu /home/ubuntu/
