acd72b5c006e5f96449cc4cb50ead2c86e47289b
John Vial <john.francis.vial@gmail.com>
2016-01-28 15:37:45 +1000

    add to sudeors

--- sl.sh
+++ sl.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 adduser --disabled-password --gecos "" ubuntu
+adduser ubunutu sudo
 mkdir /home/ubuntu/.ssh/
 cp /root/.ssh/authorized_keys /home/ubuntu/.ssh/authorized_keys
 chown -R ubuntu /home/ubuntu/
